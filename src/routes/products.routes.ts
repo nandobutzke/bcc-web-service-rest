f7b943af43758b78aa794c59ce01151b26f4b678
Fernando Butzke <nandoproject209@gmail.com>
2021-08-15 00:04:03 -0300

    Resolving import

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -2,8 +2,6 @@ import { Router } from 'express';
 
 const productsRouter = Router();
 
-const products = [];
-
 productsRouter.post('/', (request, response) => {
     return response.json({ message: 'Teste' });
 });
