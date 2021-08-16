462313017ed559bbb1633f951933e7822ee755c3
Fernando Butzke <nandoproject209@gmail.com>
2021-08-16 20:12:51 -0300

    up

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -2,8 +2,16 @@ import { Router } from 'express';
 
 const productsRouter = Router();
 
-productsRouter.post('/', (request, response) => {
-    return response.json({ message: 'Teste' });
+productsRouter.post('/', (req, res) => {
+    const products = [
+        { id: 1, name: 'tênis' },
+        { id: 2, name: 'camisa' },
+        { id: 3, name: 'boné' },
+    ];
+
+    console.log(req.body)
+
+    return res.json(products);
 });
 
 export default productsRouter;
